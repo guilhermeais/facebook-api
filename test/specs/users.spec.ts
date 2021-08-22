f20e15a7e3af4de335b7e03e789e4c6380f30b4b
Guilhermeais <guilhermeteixeiraais@gmail.com>
2021-08-22 16:46:22 -0300

    .

--- test/specs/users.spec.ts
+++ test/specs/users.spec.ts
@@ -1,36 +1,36 @@
-import test from "japa";
-import { request } from "Test/utils";
-import { UserFactory } from "Database/factories";
-import Mail from "@ioc:Adonis/Addons/Mail";
-import Database from "@ioc:Adonis/Lucid/Database";
-import faker from "faker";
+import test from 'japa'
+import { request } from 'Test/utils'
+import { UserFactory } from 'Database/factories'
+import Mail from '@ioc:Adonis/Addons/Mail'
+import Database from '@ioc:Adonis/Lucid/Database'
+import faker from 'faker'
 
-test.group("/users/register", (group) => {
+test.group('/users/register', (group) => {
   group.beforeEach(async () => {
-    await Database.beginGlobalTransaction();
-  });
+    await Database.beginGlobalTransaction()
+  })
 
   group.afterEach(async () => {
-    await Database.rollbackGlobalTransaction();
-  });
+    await Database.rollbackGlobalTransaction()
+  })
 
-  test("[store] - should able to send mail after pre-registration", async (assert) => {
-    const email = faker.internet.email();
+  test('[store] - should able to send mail after pre-registration', async (assert) => {
+    const email = faker.internet.email()
 
     Mail.trap((message) => {
-      assert.deepEqual(message.to, [{ address: email }]);
-      assert.deepEqual(message.subject, "Criação de conta");
+      assert.deepEqual(message.to, [{ address: email }])
+      assert.deepEqual(message.subject, 'Criação de conta')
       assert.deepEqual(message.from, {
-        address: "contato@facebook.com",
-        name: "Facebook",
-      });
-    });
+        address: 'contato@facebook.com',
+        name: 'Facebook',
+      })
+    })
 
     await request
-      .post("/users/register")
-      .send({ email, redirectUrl: "https://facebook.com" })
-      .expect(200);
+      .post('/users/register')
+      .send({ email, redirectUrl: 'https://facebook.com' })
+      .expect(200)
 
-    Mail.restore();
-  });
-});
+    Mail.restore()
+  })
+})
